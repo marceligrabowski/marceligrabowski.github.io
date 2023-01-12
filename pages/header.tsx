6970ead8e1c0e0639ddd080b3569bfff9c4af1a1
Marceli Grabowski <marceli@marceligrabowski.pl>
2023-01-12 22:24:48 +0000

    navbar

--- pages/header.tsx
+++ pages/header.tsx
@@ -1,30 +1,49 @@
-import { Button, Navbar } from "flowbite-react";
+import { Avatar, Button, Navbar } from "flowbite-react";
+import { useRouter } from "next/router";
+import { AiFillLinkedin, AiOutlineGithub } from "react-icons/ai";
 
 export default function Header() {
+  const { asPath } = useRouter();
+
   return (
-    <Navbar fluid={true} rounded={true}>
-      <Navbar.Brand href="https://flowbite.com/">
-        <img
-          src="https://flowbite.com/docs/images/logo.svg"
-          className="mr-3 h-6 sm:h-9"
-          alt="Flowbite Logo"
-        />
+    <Navbar fluid={true}>
+      <Navbar.Brand
+        className="flex-grow"
+        href="https://marceligrabowski.github.io/"
+      >
+        <Avatar
+          className="mr-3"
+          img="mua.jpg"
+          rounded={true}
+          size="lg"
+        ></Avatar>
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-          Flowbite
+          Marceli Grabowski | Software Developer
         </span>
       </Navbar.Brand>
       <div className="flex md:order-2">
-        <Button>Get started</Button>
+        <Button
+          className="m-1"
+          href="https://www.linkedin.com/in/marceligrabowski/"
+        >
+          <AiFillLinkedin className="h-6 w-6" />
+        </Button>
+        <Button className="m-1" href="https://github.com/marceligrabowski">
+          <AiOutlineGithub className="h-6 w-6" />
+        </Button>
         <Navbar.Toggle />
       </div>
-      <Navbar.Collapse>
-        <Navbar.Link href="/navbars" active={true}>
-          Home
+
+      <Navbar.Collapse className="md:m-4">
+        <Navbar.Link href="/" active={asPath === "/"}>
+          Blog
         </Navbar.Link>
-        <Navbar.Link href="/navbars">About</Navbar.Link>
-        <Navbar.Link href="/navbars">Services</Navbar.Link>
-        <Navbar.Link href="/navbars">Pricing</Navbar.Link>
-        <Navbar.Link href="/navbars">Contact</Navbar.Link>
+        <Navbar.Link href="/about-me" active={asPath === "/about-me"}>
+          About me
+        </Navbar.Link>
+        <Navbar.Link href="/resume" active={asPath === "/resume"}>
+          Resume
+        </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
   );
